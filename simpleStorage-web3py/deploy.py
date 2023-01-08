f81ce352fc48bc7142f37948ed259dd0a47f8da4
dvzhang <dvzhangtz@sina.com>
2023-01-07 16:25:50 -0800

    1.7 commit

--- simpleStorage-web3py/deploy.py
+++ simpleStorage-web3py/deploy.py
@@ -1,5 +1,6 @@
 from solcx import  compile_standard
 import json
+from web3 import Web3
 
 # import solcx
 # solcx.install_solc('0.6.0')
@@ -9,7 +10,7 @@ with open("./SimpleStorage.sol", "r") as file:
     # print(simple_storage_file)
 
 
-compile_sol = compile_standard(
+compiled_sol = compile_standard(
     {
         "language": "Solidity",
         "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
@@ -26,4 +27,40 @@ compile_sol = compile_standard(
 
 
 with open("compiled_code.json", "w") as file:
-    json.dump(compile_sol, file)
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+chain_id = 1337
+my_address = "0x4A725C718b656e471Fe5603c5ce5159B2ebF429B"
+private_key = "c00047b3e60b1af5d62168f3c725679f17938c7da5a7b99adc27608e710f38d8"
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
