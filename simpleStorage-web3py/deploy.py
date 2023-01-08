4b2f7add4e53e619a92aa28e2e24fc482a1630a5
dvzhang <dvzhangtz@sina.com>
2023-01-07 23:36:10 -0800

    1.7 commit

--- simpleStorage-web3py/deploy.py
+++ simpleStorage-web3py/deploy.py
@@ -2,6 +2,10 @@ from solcx import  compile_standard
 import json
 from web3 import Web3
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # import solcx
 # solcx.install_solc('0.6.0')
@@ -38,12 +42,10 @@ abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
 
-w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.0:8545"))
 chain_id = 1337
-# my_address = os.getenv("MY_ADDRESS")
-# private_key = os.getenv("PRIVATE_KEY")
-my_address = "0x4A725C718b656e471Fe5603c5ce5159B2ebF429B"
-private_key = "c00047b3e60b1af5d62168f3c725679f17938c7da5a7b99adc27608e710f38d8"
+my_address = os.getenv("MY_ADDRESS")
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -67,3 +69,26 @@ tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+print(simple_storage.functions.retrieve().call())
+
+# greeting_transaction = simple_storage.functions.store(15).call()
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
+        {
+            "chainId": chain_id,
+            "gasPrice": w3.eth.gas_price,
+            "from": my_address,
+            "nonce": nonce + 1,
+        }
+    )
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
+)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
+
+print(simple_storage.functions.retrieve().call())
